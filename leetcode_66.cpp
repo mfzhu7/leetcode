f374a4cf86792405a53a4955443408f0d5cb3cdf
your name <553232840@qq.com>
2022-12-03 16:12:26 +0800

    删除多余文件

--- leetcode_66.cpp
+++ leetcode_66.cpp
@@ -1,26 +1,41 @@
+#include<vector>
+
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        if (digits[digits.size()-1] != 9){
-            digits[digits.size()-1] = digits[digits.size()-1] + 1;
-            return digits;
+        int add = 0;
+        bool tag = true;
+        int place = digits.size() - 1;
+        while((tag || add) && place >= 0 ){
+            int temp = digits[place] + 1;
+            add = (temp == 10) ? 1 : 0;
+            temp = temp % 10;
+            digits[place] = temp;
+            tag = false;
+            place = place - 1;
         }
-
-        long long  temp = 1;
-        long long total = 0;
-        for (int i = digits.size() - 1; i >= 0; i--){
-            total = total + temp * digits[i];
-            temp = temp * 10;
+        if(add){
+            digits.emplace(digits.begin(), 1);
         }
-        total = total + 1;
-        vector<int> vec; 
-        while(total) {
-            int temp = total % 10;
-            vec.push_back(temp);
-            total = (total - temp) / 10;
-        }
-        reverse(vec.begin(), vec.end());
-        return vec;
-
+        return digits;
     }
 };
+
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        for (int i = digits.size() - 1; i >= 0; i--){
+            if (digits[i] == 9){
+                digits[i] = 0;
+            }else {
+                digits[i] = digits[i] + 1;
+                return digits;
+            }
+        }
+        digits.emplace(digits.begin(), 1);
+        return digits;
+    }
+};
+
+
