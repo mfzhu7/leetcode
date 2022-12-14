b623a78fbda5b6fb6bfe24e1937308817b59211e
your name <553232840@qq.com>
2022-12-14 22:41:11 +0800

    'leetcode'

--- leetcode_73.cpp
+++ leetcode_73.cpp
@@ -1,22 +1,3 @@
-#include<iostream>
-#include<vector>
-#include<string>
-#include<algorithm>
-#include<map>
-#include<set>
-
-
-using namespace std;
-
-void print(vector<int> nums){
-    for (auto i: nums){
-        cout << i << " ";
-    }
-    cout << endl;
-    return;
-}
-
-
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
@@ -33,20 +14,10 @@ public:
         for (int i = 0; i < matrix.size(); i++){
             for (int j = 0; j < matrix[0].size(); j++){
                 if (row.find(i) != row.end() || col.find(j) != col.end()){
-                    matrix[i][j] == 0;
-                    cout << i << j << endl;
+                    matrix[i][j] = 0;
                 }
             }
         }
         return;
     }
 };
-
-
-int main(){
-    vector<vector<int>> vec = {
-        {1,1,1},{1,0,1},{1,1,1}
-    };
-    Solution test;
-    test.setZeroes(vec);
-}
