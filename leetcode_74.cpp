6086ca59890a1aa7dc67bc7fed3db011e361752e
your name <553232840@qq.com>
2022-12-15 22:38:01 +0800

    '修复'

--- leetcode_74.cpp
+++ leetcode_74.cpp
@@ -30,3 +30,24 @@ public:
 
     }
 };
+
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int row = matrix.size();
+        int col = matrix[0].size();
+        int i = 0;
+        int j = col - 1;
+        while(i < row && j >= 0){
+            if (matrix[i][j] == target) return true;
+            if (matrix[i][j] > target){
+                j = j - 1;
+            } else {
+                i = i + 1;
+            }
+        }
+        return false;
+
+    }
+};
