1363dfeb9fb965fa5257eb97e58ef10399c7adb8
your name <553232840@qq.com>
2022-12-24 13:35:41 +0800

    'leetcode'

--- leetcode_36.cpp
+++ leetcode_36.cpp
@@ -44,3 +44,30 @@ public:
 
     }
 };
+
+
+
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        int row[9][10] = {0};
+        int col[9][10] = {0};
+        int box[9][10] = {0};
+        
+        for (int i = 0; i < 9; i++){
+            for (int j = 0; j < 9; j++){
+                if (board[i][j] == '.') continue;
+                int currNum = board[i][j] - '0';
+                if(row[i][currNum]) return false;
+                if(col[j][currNum]) return false;
+                if(box[j/3+ (i/3)*3][currNum]) return false;
+                row[i][currNum] = 1;
+                col[j][currNum] = 1;
+                box[j/3+ (i/3)*3][currNum] = 1;
+
+            }
+        }
+        return true;
+
+    }
+};
