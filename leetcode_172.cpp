ec15984242d47c090f5a46c2ab586419db07b152
your name <553232840@qq.com>
2022-12-31 13:52:31 +0800

    leetcode

--- leetcode_172.cpp
+++ leetcode_172.cpp
@@ -13,8 +13,20 @@ public:
         for (int i = 0; i <= n; i++){
             if (i % 5 == 0){
                 int temp = count(i);
-                cout << temp << endl;
                 ret = ret + temp;
+            }
+        }
+        return ret;
+    }
+};
+
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        int ret = 0;
+        for (int i = 5; i <= n; i = i + 5){
+            for (int j = i; j % 5 == 0; j = j / 5){
+                ret = ret + 1;
             }
         }
         return ret;
