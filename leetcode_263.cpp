93b0babf6e168918eb978aa24344c0d6d5320475
your name <553232840@qq.com>
2023-01-08 15:22:47 +0800

    leetcode

---
+++ leetcode_263.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool isUgly(int n) {
+        if (n <= 0) return false;
+        while(n){
+            if (n % 2 == 0) n = n / 2;
+            else if  (n % 3 == 0){
+                n = n / 3;
+            } else if (n % 5 == 0){
+                n = n / 5;
+            } else {
+                if (n == 1) return true;
+                else return false;
+            }
+        }
+        return true;
+
+    }
+};
+
+class Solution {
+public:
+    bool isUgly(int n) {
+        if (n <= 0) return false;
+        vector<int> vec {2,3,5};
+        for (auto i: vec){
+            while(n % i == 0){
+                n = n / i;
+            }
+        }
+        return n == 1;
+
+    }
+};
