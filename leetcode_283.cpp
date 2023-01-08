1c50405a06c4178eac7a86d4ce78c4d83a35f6f5
your name <553232840@qq.com>
2023-01-08 15:31:59 +0800

    leetcode

---
+++ leetcode_283.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j = 0;
+        for (int i = 0; i < nums.size(); i++){
+            if(nums[i] != 0){
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        for (int i = j; i < nums.size(); i++){
+            nums[i] = 0;
+        }
+        return;
+
+    }
+};
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j = 0;
+        for (int i = 0; i < nums.size(); i++){
+            if (nums[i] != 0){
+                int tmp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = tmp;
+                j++;
+            }
+        }
+        return;
+
+    }
+};
