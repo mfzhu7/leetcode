a2b7ad1853bd9385bf6ae7d50c7d8659eb76e374
your name <553232840@qq.com>
2023-01-10 08:41:12 +0800

    leetcode

--- leetcode_223.cpp
+++ leetcode_223.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
     int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
-        int area1 = (ax2 - ax1) * (ay2 - ay1), area2 = (bx2 - bx1) * (by2 - by1);
-        int overlapWidth = min(ax2, bx2) - max(ax1, bx1), overlapHeight = min(ay2, by2) - max(ay1, by1);
-        int overlapArea = max(overlapWidth, 0) * max(overlapHeight, 0);
-        return area1 + area2 - overlapArea;
+        int area1 = (ax2 - ax1 ) * (ay2 - ay1);
+        int area2 = (bx2 - bx1) * (by2 - by1);
+
+        int overlapWidth = min(bx2, ax2) - max(bx1, ax1);
+        int overlapHeight = min(by2, ay2) - max(by1, ay1);
+
+        int overlapArea = max(overlapHeight, 0) * max(overlapWidth, 0);
+        return (area1 + area2) - overlapArea;
     }
 };
