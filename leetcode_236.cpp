52dd59893956edca3a2d4c2170152daf0a45401f
your name <553232840@qq.com>
2023-02-14 20:33:09 +0800

    leetcode

--- leetcode_236.cpp
+++ leetcode_236.cpp
@@ -36,3 +36,27 @@ public:
         
     }
 };
+
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root == nullptr || p == root || q == root){
+            return root;
+        }
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+
+        if(left == nullptr && right == nullptr){
+            return nullptr;
+        }
+        if(left == nullptr){
+            return right;
+        }
+        if(right == nullptr){
+            return left;
+        }
+        return root;
+        
+    }
+};
---
+++ leetcode_279.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int numSquares(int n) {
+        vector<int> dp(n+1);
+        dp[0] = 0;
+
+        for (int i = 1; i <= n; i++){
+            int minn = INT_MAX;
+            for(int j = 1; j * j <= i; j++){
+                minn = min(minn, dp[i - j*j]);
+            }
+            dp[i] = minn + 1;
+        }
+        return dp[n];
+
+    }
+};
