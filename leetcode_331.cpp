28b5640ffae24b252c2b9abc297ac7f6cc5fa519
your name <553232840@qq.com>
2023-02-15 22:36:01 +0800

    leetcode

--- leetcode_331.cpp
+++ leetcode_331.cpp
@@ -1,39 +1,26 @@
 class Solution {
 public:
-    bool isValidSerialization(string preorder) {
-        if(preorder == "#") return true;
-        stringstream ss(preorder);
-        string s;
-        vector<string> vec;
-        while(getline(ss, s, ',')){
-            vec.push_back(s);
-        }
-
-        stack<char> stk;
-        for (int i = 0; i < vec.size(); i++){
-            if(isdigit(vec[i][0])){
-                stk.push(vec[i][0]);
-            } else {
-                if(stk.empty()) return false;
-                if(stk.top() == '#'){
-                    while(!stk.empty() && stk.top() == '#'){
-                        stk.pop();
-                        stk.pop();
-                    }
-                    if(!stk.empty()){
-                        stk.push(vec[i][0]);
-                    } else {
-                        if(i == vec.size() - 1){
-                            return true;
-                        } else {
-                            return false;
-                        }
-                    }     
-                } else {
-                    stk.push(vec[i][0]);
-                }
+    bool isValidSerialization(string preorder) 
+    {
+        vector<string> stk;         // 使用一个数组模拟栈
+        stringstream ss(preorder);  // 使用 stringstream与getline 分割字符串
+        string temp;
+        // getline（输入流，暂存从流中读取的字符串，读取终止符）
+        while (getline(ss, temp, ','))
+        {
+            stk.push_back(temp);
+            int len = stk.size();
+            // 用「#」替换 「数字##」
+            while (len >= 3 && stk[len - 1] == "#" && stk[len - 2] == "#" && stk[len - 3] != "#")
+            {
+                stk.pop_back();
+                stk.pop_back();
+                stk.pop_back();
+                stk.push_back("#");
+                len = stk.size();
             }
         }
-        return stk.empty();
+        // 如果最后模拟栈中只剩一个#，说明是合法的序列
+        return stk.size() == 1 && stk[0] == "#";
     }
 };
