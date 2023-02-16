a4e30d92f25b0e67859c9d3583e72520c6335b66
your name <553232840@qq.com>
2023-02-16 23:19:46 +0800

    leetcode

---
+++ leetcode_300.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n, 0);
+        dp[0] = 1;
+        int ans = 1;
+        for (int i = 1; i < n; i++){
+            int temp = 1;
+            for (int j = 0; j < i; j++){
+                if(nums[i] > nums[j]){
+                    temp = max(temp, dp[j] + 1);
+                }
+            }
+            dp[i] = temp;
+            ans = max(ans, dp[i]);
+        }
+        return ans;
+    }
+};
