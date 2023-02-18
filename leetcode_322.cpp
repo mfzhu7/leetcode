255ffb4059a754aee90b4ef6cf7d734fbf3ec9d3
your name <553232840@qq.com>
2023-02-18 19:26:28 +0800

    leetcode

--- leetcode_322.cpp
+++ leetcode_322.cpp
@@ -21,3 +21,24 @@ public:
         return minn;
     }
 };
+
+
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+            // sort(coins.begin(), coins.end());
+            int MAX = amount + 1;
+            vector<int> dp(amount + 1, MAX);
+            dp[0] = 0;
+            for (int i = 1;i <= amount; i++){
+                for (int j = 0; j < (int)coins.size(); j++){
+                    if(i >= coins[j]){
+                        dp[i] = min(dp[i - coins[j]] + 1, dp[i]);
+                    }
+                }
+            }
+
+            return dp[amount] > amount? -1: dp[amount]; 
+
+    }
+};
